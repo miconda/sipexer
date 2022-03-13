d72989e951315c8365772f5d1053f5888cff0994
Daniel-Constantin Mierla <miconda@gmail.com>
2022-03-13 17:28:41 +0100

    add $pid variable to return process id

--- sipexer.go
+++ sipexer.go
@@ -620,6 +620,8 @@ func main() {
 					tplfields[k] = time.Now().Format(time.ANSIC)
 				} else if tplfields[k] == "$timestamp" {
 					tplfields[k] = strconv.FormatInt(time.Now().Unix(), 10)
+				} else if tplfields[k] == "$pid" {
+					tplfields[k] = strconv.Itoa(os.Getpid())
 				} else if tplfields[k] == "$cr" {
 					tplfields[k] = "\r"
 				} else if tplfields[k] == "$lf" {
