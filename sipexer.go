cd4bb654499bcb5586c54e99a8ae6985014896fd
Daniel-Constantin Mierla <miconda@gmail.com>
2025-11-08 20:27:32 +0100

    sipexer: fields to provide full From/To header body

--- sipexer.go
+++ sipexer.go
@@ -94,8 +94,8 @@ const (
 
 var templateDefaultText string = `{{.method}} {{.ruri}} SIP/2.0
 Via: SIP/2.0/{{.viaproto}} {{.viaaddr}}{{.rport}};{{.viaparams}}branch=z9hG4bKSG.{{.viabranch}}
-From: {{if .fromuri}}{{.fromuri}}{{else}}{{if .fname}}"{{.fname}}" {{end}}<sip:{{if .fuser}}{{.fuser}}@{{end}}{{.fdomain}}>{{end}}{{if .fromtag}};tag={{.fromtag}}{{end}}
-To: {{if .touri}}{{.touri}}{{else}}{{if .tname}}"{{.tname}}" {{end}}<sip:{{if .tuser}}{{.tuser}}@{{end}}{{.tdomain}}>{{end}}{{if .totag}};tag={{.totag}}{{end}}
+From: {{if .fromhdr}}{{.fromhdr}}{{else}}{{if .fromuri}}{{.fromuri}}{{else}}{{if .fname}}"{{.fname}}" {{end}}<sip:{{if .fuser}}{{.fuser}}@{{end}}{{.fdomain}}>{{end}}{{if .fromtag}};tag={{.fromtag}}{{end}}{{end}}
+To: {{if .tohdr}}{{.tohdr}}{{else}}{{if .touri}}{{.touri}}{{else}}{{if .tname}}"{{.tname}}" {{end}}<sip:{{if .tuser}}{{.tuser}}@{{end}}{{.tdomain}}>{{end}}{{if .totag}};tag={{.totag}}{{end}}{{end}}
 Call-ID: {{.callid}}
 CSeq: {{.cseqnum}} {{.method}}
 {{if .subject}}Subject: {{.subject}}{{else}}$rmeol{{end}}
