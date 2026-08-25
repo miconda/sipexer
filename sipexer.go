60c8678fe322052dd3c3042bdf3669b8721e69f9
Daniel-Constantin Mierla <miconda@gmail.com>
2026-08-25 10:57:50 +0200

    sipexer: new cseq number for invite in --call-user

--- sipexer.go
+++ sipexer.go
@@ -1702,6 +1702,7 @@ func SIPExerRunCallUsers(dstSockAddr sgsip.SGSIPSocketAddress, wsurlp *url.URL, 
 	invFields := SIPExerCloneTplFields(baseTplFields)
 	invFields["method"] = "INVITE"
 	invFields["callid"] = uuid.New().String()
+	invFields["cseqnum"] = strconv.Itoa(1 + mathrand.Intn(999999))
 	invFields["tuser"] = cliops.u2fuser
 	var dstURI sgsip.SGSIPURI
 	sgsip.SGSocketAddressToSIPURI(&dstSockAddr, cliops.u2fuser, 0, &dstURI)
